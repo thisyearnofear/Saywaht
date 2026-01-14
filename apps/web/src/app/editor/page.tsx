7e6d9ccbd938c37213bfa8df44efaed16d7d94f8
Quincybob <74205675+Quincybob@users.noreply.github.com>
2026-01-14 15:54:27 +0300

    fix: move 'use client' to top of files to fix build errors

--- apps/web/src/app/editor/page.tsx
+++ apps/web/src/app/editor/page.tsx
@@ -1,6 +1,6 @@
-export const dynamic = "force-dynamic";
-
 "use client";
+
+export const dynamic = "force-dynamic";
 
 import nextDynamic from "next/dynamic";
 // import { Suspense } from 'react';
--- apps/web/src/app/trade/page.tsx
+++ apps/web/src/app/trade/page.tsx
@@ -1,6 +1,6 @@
-export const dynamic = "force-dynamic";
-
 "use client";
+
+export const dynamic = "force-dynamic";
 
 import { TradingFeed } from "@/components/trading/trading-feed";
 import { WalletGuard } from "@/components/wallet-guard";
